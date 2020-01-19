43d7a6f575979996a19258ac8c10985a16b13a84
shihang <shihang.yu@inceptioniai.org>
2020-01-19 11:29:17 +0400

    experiment of split sentence and merge

--- json_server.py
+++ json_server.py
@@ -2,7 +2,7 @@ import sys
 import os
 import argparse
 import uuid
-import time
+import re
 import librosa
 sys.path.append('waveglow/')
 from pathlib import Path
@@ -101,6 +101,92 @@ def synthesize():
     logger.debug(f"save time {time.time() - t4}")
     logger.debug(f"total time {time.time() - t1}")
     logger.debug(f'input length {len(text)}, ratio {(time.time()-t1)/len(text)}')
+
+    @after_this_request
+    def remove_file(response):
+        torch.cuda.empty_cache()
+        try:
+            os.remove(whole_path_wav)
+        except Exception as error:
+            logger.error(f"Error removing file {whole_path_wav}")
+
+        if ext != 'wav':
+            try:
+                os.remove(whole_path_ext)
+            except Exception as error:
+                logger.error(f"Error removing file {whole_path_ext}")
+
+        return response
+
+    return send_file(whole_path_ext, mimetype=f'audio/{ext}')
+
+# split and synthesize for the long sentence, only use in experiment
+@app.route('/synthesize_split', methods=['GET', 'POST'])
+def synthesize_split():
+    def split(long_text):
+        return re.findall(r'.{1,100}(?:\s+|$)', long_text)
+
+    t1 = time.time()
+    # cut the longer syntax, can stop memory grow
+    long_text = request.values.get('text').strip()
+    logger.info(f'input text: {long_text}')
+
+    text_list = split(long_text)
+
+    audio_denoised_list = []
+
+    for text in text_list:
+        sequence = np.array(text_to_sequence(text, ['enhanced_english_cleaners'], verbose=True))[None, :]
+        sequence = torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
+
+        torch.cuda.synchronize()
+        t2 = time.time()
+        logger.debug(f"preprocess time {t2 - t1}")
+
+        try:
+            with torch.no_grad():
+                mel_outputs_postnet = model.inference_simple(sequence)
+                torch.cuda.empty_cache()
+                audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+                torch.cuda.empty_cache()
+
+                torch.cuda.synchronize()
+                t3 = time.time()
+                logger.debug(f"inference time {t3 - t2}")
+
+                audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+
+                torch.cuda.synchronize()
+                t4 = time.time()
+                logger.debug(f"denoise time {t4 - t3}")
+
+        except RuntimeError as e:
+            if e.args[0].startswith('CUDA out of memory'):
+                logger.fatal('GPU OUT OF MEMORY!')
+
+            torch.cuda.empty_cache()
+            return None
+
+        audio_denoised_list.append(audio_denoised)
+
+    audio_denoised_long = torch.cat((audio_denoised_list), 1) # concatenate all the splited audios
+    wav_name = f'{str(uuid.uuid1())}'
+    whole_path_wav = result_path /  f'{wav_name}.wav'
+    librosa.output.write_wav(str(whole_path_wav), audio_denoised_long.cpu().numpy().T, hparams.sampling_rate)
+
+    if ext == 'wav':
+        whole_path_ext = whole_path_wav
+    elif ext == 'mp3':
+        whole_path_ext = result_path /  f'{wav_name}.mp3'
+        # if convert fail, return name fall back to whole_path_wav
+        whole_path_ext = save_mp3_use_ffmpeg(hparams.sampling_rate, mp3_bitrate, whole_path_wav, whole_path_ext)
+    else:
+        return RuntimeError("unknown file extension")
+
+        torch.cuda.synchronize()
+        logger.debug(f"save time {time.time() - t4}")
+        logger.debug(f"total time {time.time() - t1}")
+        logger.debug(f'input length {len(text)}, ratio {(time.time()-t1)/len(text)}')
 
     @after_this_request
     def remove_file(response):
